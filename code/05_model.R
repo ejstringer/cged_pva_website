11c1d30e0e6c27b9dfa4a3a09db950858c195fc4
Emily Stringer <stringer@duncan.win.canberra.edu.au>
2025-04-16 11:04:26 +1000

    tidy up model

--- code/05_model.R
+++ code/05_model.R
@@ -1,33 +1,47 @@
 
 # funtions -------------------------------------------------
 
+# logit and unlogit functions
 logit <- function(p) log(p/(1-p))
 unlogit <- function(x) exp(x) / (1 + exp(x))
 
-em.initial.N <- function(init, stage_distribution, p){
+
+# initialise starting population size across stages based on stable stage
+# distributions derived from best guess stage matrix. initial population size
+# is estimated for adults and sub-adults only, so juvenile abundance added.
+
+em.initial.N <- function(init_A_SA,               
+                         stage_distribution, 
+                         population_names){                 
   
-  adult.stages <- stage_distribution[-1]
-  juv.stage <- stage_distribution[1]
+  # summaries adult stable stages and juvenile stage
+  adult.stages <- stage_distribution[-1] 
+  adult.stages.proportion <- adult.stages/sum(adult.stages)
+  juv.stage <- stage_distribution[1]              
   
-  initial_abundance <- round(rbind(J=(init/(1-juv.stage))*juv.stage,
-              sapply(init, function(x) (adult.stages/sum(adult.stages))*x)))
+  # Calculate N
+  total_N <- init_A_SA/(1-juv.stage)
+  juv_N <- (total_N)*juv.stage
+  adult_N <- sapply(init_A_SA, function(x) (adult.stages.proportion)*x)
   
-   
+  initial_abundance <- round(rbind(juv_N, adult_N)) 
+  
+  # adding names to matrix 
   rownames(initial_abundance) <- names(stage_distribution)
-  colnames(initial_abundance) <- p
+  colnames(initial_abundance) <- population_names
   initial_abundance
   
 }
 
-
+# Function to determine clutch sizes of mothers
 em.juveniles <- function(mothers, clutch_sizes){
   
   n <- length(clutch_sizes)
-  prob_seq <- round(seq(0.1,0.9, length.out = n),2)
-  beta2 <- dbeta(prob_seq,
-                 2,2)
+  prob_seq <- seq(0.1,0.9, length.out = n)
+  beta2 <- dbeta(prob_seq,2,2) 
   clutch_prob <-beta2/sum(beta2)
-  if(mothers >0){
+  
+  if(mothers >0){# necessary because mothers = 0 returns nothing
     sample(clutch_sizes,
            size = mothers,
            prob = clutch_prob,
@@ -56,30 +70,34 @@ em.pva_simulator <- function(populations = c('CA', 'JE', 'JW', 'MA'),
                              time_steps = 8, # time
                              replicates = 1){
   
+  ## initialise model ------------
+  # initial population sizes across stages and sites
   initial_abundance <- em.initial.N(initial_ab, stage_distribution, populations)
   n_stages <- nrow(initial_abundance)
   n_populations <- ncol(initial_abundance)
   
-  
+  # abundance array
   N <- array(dim = c(n_stages, n_populations, time_steps, replicates))
   N[, , 1, ] <- initial_abundance
   
   
-  # survival adjust
+  # survival adjustment for sites (calibration only)
   
   if(!is.null(site_adjust)){
     
     survival <- unlogit(sapply(site_adjust, 
-                   qnorm, mean = logit(survival), sd = survival_logit_sd))
+                   qnorm, 
+                   mean = logit(survival), 
+                   sd = survival_logit_sd))
     survival_J <- unlogit(sapply(site_adjust, 
-                               qnorm, mean = logit(survival_J), 
-                                      sd = survival_logit_sd))
+                               qnorm, 
+                               mean = logit(survival_J), 
+                               sd = survival_logit_sd))
     
   }
  
-  # for loop replicate (i)
+  
   for (i in 1:replicates) {
-    # for loop year (yr)
     for (yr in 2:time_steps) {
       
       ## survival ---------
@@ -93,36 +111,38 @@ em.pva_simulator <- function(populations = c('CA', 'JE', 'JW', 'MA'),
                                     function(x) pnorm(logit(survival_env)[x], 
                                                               logit(survival[x]),
                                                               env_stoch[x]))
-      
+      ### environmnetal stoch J - same effect as adults
       survival_J_env <- unlogit(sapply(1:n_populations, 
               function(x) qnorm(p_environment_effect[x],
                                 logit(survival_J[x]),
                                 env_stoch[x])))
       
-      
+      ### yearly environmental adjusted survival 
       survival_year <- cbind(J= survival_J_env,
                         sapply(1:(n_stages-2), function(x) survival_env),
-                        A4 = 0)
+                        oldest = 0)
       
       for (sites in 1:n_populations) {
         surv <- survival_year[sites,]
         prev.N <- N[,sites , yr-1, i]
+        # demographic stochasitcity (binomial/bernoulli distribution)
         new.N <- sapply(1:length(prev.N), function(x) sum(rbinom(prev.N[x],1,surv[x])))
         N[, sites, yr, i] <- new.N
           }
       
       ## transition ---------
+      new.tran <-  (transition_mat %*% N[, , yr, i])
       
+      # Juvenile stage transition 
+          stage_juv <- transition_mat[,1]
+          stage_Njuv <- N[1, ,yr,i]
+          juv_tran <- stage_juv[stage_juv>0]
+          # demographic stochasitcity (binomial/bernoulli distribution)
+          stage1 <- sapply(stage_Njuv, function(x) sum(rbinom(x,1, juv_tran[1])))
+          stage2 <- stage_Njuv-stage1
+          new.tran[stage_juv>0,] <- rbind(stage1, stage2)
       
-      tran.mat <- transition_mat
-      tran.mat[tran.mat<1 & tran.mat>0] <- 0
-      SAtran <- transition_mat[2,1]
-      transitioning_J <- N[1, , yr, i]
-      new.tran <-  (tran.mat %*% N[, , yr, i])
-      new.SA <- sapply(transitioning_J, function(x) sum(rbinom(x,1, SAtran)))
-      new.A <- transitioning_J-new.SA
-      new.tran[2,] <- new.SA
-      new.tran[3,] <- new.A
+          # update N
       N[, , yr, i] <- new.tran
       
       ## reproduction ---------
@@ -135,15 +155,18 @@ em.pva_simulator <- function(populations = c('CA', 'JE', 'JW', 'MA'),
       N[1, , yr, i]<- juvs
       
       
-      ## carrying capcity ---------
+      ## carrying capacity ---------
       adults <- colSums(N[2:n_stages,,yr,i])
       
       if(sum(adults > K)>0){
-      adults.dist <- stage_distribution[-1]/sum(stage_distribution[-1])
-        K.stages <- round(sapply(K, function(x) adults.dist*x))
+        sites_K <- which(adults>K)
+          
+        adult.stages <- stage_distribution[-1] 
+        adults.dist <- adult.stages/sum(adult.stages)
+        K.stages <- round(sapply(K[sites_K], function(x) adults.dist*x))
         for(age in 2:n_stages){
-          current_N <-  N[age, , yr, i]
-          N[age, , yr, i]<- ifelse(current_N > K.stages[age-1,],
+          current_N <-  N[age, sites_K, yr, i]
+          N[age, sites_K, yr, i]<- ifelse(current_N > K.stages[age-1,],
                                    K.stages[age-1,], current_N) 
         }
         
@@ -156,5 +179,23 @@ em.pva_simulator <- function(populations = c('CA', 'JE', 'JW', 'MA'),
    
   }
 
+transition_mat2 <- transition_mat
+transition_mat2[6,1] <- transition_mat2[3,1] 
+transition_mat2[3,1] <- 0
+transition_mat2[3:4,2] <- 0.5
 
-
+em.pva_simulator(populations = c('CA', 'JE', 'JW', 'MA'),
+                             stages = c('J', 'SA','A1','A2','A3', 'A4'),
+                             stage_distribution, 
+                             initial_ab, # adult abundance for populations
+                             survival, # survival of adults and SA at sites
+                             survival_J, # juvenile survival per site
+                             survival_logit_sd = NULL,
+                             site_adjust = NULL,
+                             env_stoch, # sd on survival
+                             transition_mat2, # transition prob to SA
+                             f_reproducing, # proportion of females reproducing
+                             clutch_sizes, # clutch size range   
+                             K = K, # carrying capcity applied to adults and SA
+                             time_steps = 8, # time
+                             replicates = 1)
