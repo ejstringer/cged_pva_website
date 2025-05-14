ab9684d5a75bc3e03ea059a946e45f22b53071c5
Emily Stringer <stringer@duncan.win.canberra.edu.au>
2025-05-14 12:34:11 +1000

    exploration

--- code/10_captive.R
+++ code/10_captive.R
@@ -1,4 +1,4 @@
-
+library(survival)
 # zims data -----------------------------------------------
 tid <- read.csv('./data/tidbinbilla_colony.csv')
 mel <- read.csv('./data/melbourne_colony.csv')
@@ -38,9 +38,26 @@ pred.surv <- data.frame(days.alive = x_days,
          years.alive = days.alive/365,
          months.alive = years.alive*12)
 
+table(tid_colony$status[(tid_colony$days.alive/365)<1])
+
+table(round(tid_colony$time), tid_colony$status) %>% 
+  as.matrix %>% 
+  as.data.frame() %>%
+  pivot_wider(names_from = Var2, values_from = Freq) %>% 
+  mutate(Var1 = as.numeric(as.character(Var1)),
+         Var1 = ifelse(Var1 > 9, 9, Var1)) %>% 
+  group_by(Var1) %>%
+  summarise(Alive = sum(Alive),
+            Dead = sum(dead)) %>% 
+  mutate(All = Alive + Dead,
+         survival = Alive/All,
+         Age = paste0(Var1, '-', Var1 +1, 'yrs'))
+
 pred.surv %>% 
   filter(year %in% c(1:7)) %>% 
-  mutate(stage = c('J', paste0('A', 1:6)))
+  mutate(stage = c('J', paste0('A', 1:6)),
+         survival_calc = 0.89^c(1,2,3,4,5,6,7),
+         surv_diff = abs(survival - survival_calc))
 
 tid_colony %>% 
   mutate(years.alive = days.alive/365,
@@ -52,6 +69,26 @@ ggplot(aes(y = survival, x = months.alive))+
   geom_line(data = pred.surv, colour = 'grey', lwd = 1)
 
 
+
+# survival analysis -------------------------------------------------------
+#https://rviews.rstudio.com/2017/09/25/survival-analysis-with-r/
+tid_colony$time <- tid_colony$days.alive/365
+tid_colony$event <- ifelse(tid_colony$survival == 0, 1,0)
+tid_colony$age <- ifelse(tid_colony$days.alive > (365*3), 'old', 'young')
+
+km.curve <- with(tid_colony, Surv(time, event))
+head(km.curve,80)
+
+
+km_fit <- survfit(Surv(time, event) ~ 1, data=tid_colony)
+summary(km_fit, times = c(1:7))
+
+
+plot(km_fit, xlab="Years", main = 'Kaplan Meyer Plot')
+
+km_AG_fit <- survfit(Surv(time, event) ~ age, data=tid_colony)
+summary(km_AG_fit, times = c(1:5), age = 'old')
+plot(km_AG_fit, xlab="Years", main = 'Kaplan Meyer Plot', col = 1:2)
 
 # data from ryan ----------------------------------------------------------
 
@@ -111,7 +148,7 @@ ged %>% filter(sex == 'F',
   group_by(dam.ID) %>% 
   summarise(clutches = n()) %>% arrange(clutches) 
 
-ged$Female.Clutch.Count
+table(ged$Female.Clutch.Count, ged$year)
 ged$egg.no %>% boxplot
 
 filter(ged, dam.ID == 'B90783')
@@ -154,7 +191,7 @@ AAnumber <- ged %>% select(GED.ID, AA_Number) %>%
 
 
 # save csvs ------------
-dir.create('./data/colony')
+#dir.create('./data/colony')
 
 write.csv(dragons, './data/colony/dragons.csv', row.names = FALSE)
 write.csv(clutches, './data/colony/clutches.csv', row.names = FALSE)
@@ -172,15 +209,23 @@ births$hatch.date
 momsbirth<-clutches %>% 
   left_join(dragons[,c('ged.id', 'hatch.date')], 
             by = c('dam.id' = 'ged.id')) %>% 
-  filter(complete.cases(hatch.date),
-         complete.cases(lay.date)) %>% 
+  #filter(complete.cases(hatch.date), complete.cases(lay.date)) %>% 
+  filter(!grepl('UC', clutch.id)) %>% 
   mutate(age = lay.date - hatch.date,
-         years = age/365,
+         years = as.numeric(age/365),
+         year = year(lay.date),
          months = years*12,
-         wholeyear = round(years))
-momsbirth$years %>% max
+         wholeyear = round(years),
+         not_hatched = clutch.size-number.hatched,
+         per_hatched = number.hatched/clutch.size) 
+momsbirth$years %>% max(., na.rm = T)
 plot(momsbirth$years, momsbirth$number.hatched)
 momsbirth$hatch.date %>% table
+table(round(momsbirth$years,2))
+boxplot(momsbirth$per_hatched~momsbirth$wholeyear)
+hatched_age_model <- aov(number.hatched ~ factor(wholeyear), data = momsbirth)
+summary(hatched_age_model)
+TukeyHSD(hatched_age_model)
 
 momsbirth %>%
   group_by(wholeyear) %>% 
@@ -193,3 +238,55 @@ ggplot(aes(wholeyear, hatched))+
   geom_errorbar(aes(ymin = lcl, ymax = ucl), width = 0.1)+
   geom_point(size = 2)+
   ylim(0,6)
+
+barplot(table(momsbirth$number.hatched))
+glm(number.hatched ~ (wholeyear)+I(wholeyear^2), 
+    data = momsbirth, family ='poisson') %>% summary
+
+#install.packages('pscl')
+library(pscl)
+
+summary(m1 <- zeroinfl(number.hatched ~ factor(wholeyear), data = momsbirth))
+
+summary(m1 <- zeroinfl(number.hatched ~ wholeyear + I(wholeyear^2), data = momsbirth))
+
+mnull <- update(m1, . ~ 1)
+
+pchisq(2 * (logLik(m1) - logLik(mnull)), df = 6, lower.tail = FALSE)
+AIC(m1, mnull)
+
+mean(momsbirth$per_hatched)
+
+sum(momsbirth$number.hatched)/sum(momsbirth$clutch.size)
+mean(momsbirth$clutch.size*0.68)
+
+
+(table(momsbirth$number.hatched[momsbirth$clutch.size %in% (3:8)],
+       momsbirth$wholeyear[momsbirth$clutch.size %in% (3:8)]))
+table(momsbirth$number.hatched)
+barplot(table(momsbirth$number.hatched)[-1])
+hist(momsbirth$not_hatched)
+mean(momsbirth$not_hatched)
+table(momsbirth$not_hatched>0)
+barplot(table(momsbirth$clutch.size)[2:7])
+table(momsbirth$clutch.size, momsbirth$number.hatched)
+
+clutch_size_prop <- table(momsbirth$clutch.size)[2:7]
+barplot(clutch_size_prop/sum(clutch_size_prop), ylim = c(0, 0.4))
+
+momsbirth$clutch.size %>% table
+shapiro.test(momsbirth$years)
+
+
+tb_clutches <- table(momsbirth$dam.id[grepl('TNR', momsbirth$clutch.id)], 
+      momsbirth$year[grepl('TNR', momsbirth$clutch.id)])
+
+tb_clutches <- table(momsbirth$dam.id, 
+                     momsbirth$year)
+round(table(tb_clutches)[-c(1,5)]/sum(table(tb_clutches)[-c(1,5)]),4) 
+ barplot(table(tb_clutches)[-1]/sum(table(tb_clutches)[-1]))
+apply(tb_clutches, 2, function(x) table(x[x>0]))
+px <- seq(0,100, 1)
+plot(px/10,dpois(px, lambda = 15), type = 'l')
+
+barplot(table(rpois(1000, lambda = 2))[-1])
