0bd5826b2cbe304826de283c4767ad8b8cc1a2b1
Emily Stringer <stringer@duncan.win.canberra.edu.au>
2025-07-17 14:40:43 +1000

    extinction prob supp

--- code/10_supplement.R
+++ code/10_supplement.R
@@ -1,5 +1,6 @@
 figprefix <- ''
 source('./code/00_libraries.R')
+library(mgcv)
 source('./code/05_model.R')
 # load --------------------------------------------------------------------
 
@@ -17,7 +18,7 @@ grasslands <- c('CA', 'JE', 'JW', 'MA')
 initial_N <-round(param_selecteddf$mean[grepl('N_init', param_selecteddf$name)])
 phiASA  <-  param_selecteddf$mean[grepl('survival_ASA', param_selecteddf$name)]
 phiJ  <-  param_selecteddf$mean[grepl('survival_J', param_selecteddf$name)]
-fecundity <- rep(param_selecteddf$mean[grepl('F_', param_selecteddf$name)],4)
+fecundity <- rep(base_params$fecundity,4) #rep(param_selecteddf$mean[grepl('F_', param_selecteddf$name)],4)
 K <- base_params$K
 clutch_sizes <- base_params$clutch
 environmental <- param_envdf$mean
@@ -25,84 +26,8 @@ environmental <- param_envdf$mean
 
 area_ntg$site <- grasslands 
 # # supplementation ---------------------------------------------------------
-# supplement <- T
-# total_suplemented <- 100
-# number_suplementation <- rep(total_suplemented/4,4)
-# seq_suplementation <- c(5,10,15)
-# 
-# em.sup.distribution <- function(N_supp, n.pops = 4, precision = 1){
-#  
-#   N_dist <- numeric(n.pops)
-#   for (i in 1:(n.pops-1)){
-#     N_dist[i] <- round(rnorm(1, N_supp/n.pops, precision))
-#     N_supp <- N_supp-N_dist[i]
-#   } 
-#   N_dist[n.pops] <- N_supp
-#   N_dist[sample(1:n.pops, n.pops)]
-# }
-# em.sup.distribution(100, 4,10)
-# par(mfrow = c(2,2))
-# lapply(1:4, function(x) hist(sapply(rep(100,1000), em.sup.distribution, 
-#                                     precision = 10)[x,],
-#                              breaks = 10))
-# reps <- 10000
-# 50*1000
-# total_suplemented <- (seq(12,500, 4))
-# length(total_suplemented)
-# 
-# supplement_distribution<- t(sapply(rep(4,reps), em.sup.distribution))
-# colnames(supplement_distribution) <- paste('sup', grasslands, sep = '_')
-# supplement_sample <- list()
-# for (i in 1:length(total_suplemented)) {
-#   supplement_distribution*total_suplemented[i]
-# supplement_distribution$N_supp <- total_suplemented[i]  
-# supplement_sample[[i]] <- supplement_distribution
-# }
-# 
-# supplement_sample_df <- do.call('rbind', supplement_sample)
-# head(supplement_sample_df) 
-# nrow(supplement_sample_df)
-
-# supp take two -----------------------------------------------------------
-# 
-# em.sup_across_pops <- function(n.supp = 100, n.pops = 4){
-#   x <- seq(0,n.supp, length.out = 6)
-#   x_round <- round(x)
-#   
-#   if (sum(!(x == x_round))>0) print('rounded x') 
-#   if (sum(!(x == x_round))>0) x_round <- unique(x_round)
-#   
-#   xlist <- list()
-#   for (i in 1:4) xlist[[i]] <- x_round
-#   all_combs <- expand.grid(xlist)
-#   
-#   all_combs1 <- all_combs[rowSums(all_combs)==n.supp,]
-#   
-#   all_combs1
-# }
-# seq(0,100,2)
-# em.sup_across_pops(20,4) %>% nrow()
-# supplement_sample <- lapply(c(20,40,60,80,100,200,300,400, 500, 600), em.sup_across_pops)
-# sum(sapply(supplement_sample, nrow))
-# 
-# 
-# 
-# supplement_sample_df <- do.call('rbind', supplement_sample) %>% 
-#   data.frame(n = 1:(sum(sapply(supplement_sample, nrow))*100), .,
-#            row.names = NULL) %>% 
-#   select(-n)
-# 
-# nrow(supplement_sample_df)
-# set.seed(13457)
-# rownames(supplement_sample_df) <- paste0(
-#   stri_rand_strings(nrow(supplement_sample_df), 4, "[A-Z]"),
-#   stri_rand_strings(nrow(supplement_sample_df), 4, "[0-9]"))
-# 
-# colnames(supplement_sample_df) <- grasslands
-# supplement_sample_df
-
-# take three ----------------
-n_rep<- 30
+## take three ----------------
+n_rep<- 100
 df_supp <- data.frame(rep(1:200, n_rep), 
                       rep(1:200, n_rep),
                       rep(1:200, n_rep),
@@ -125,7 +50,7 @@ supplement <- T
 seq_suplementation <- seq(14,t_steps, supp_every)
 
 supplement_sample_df <- df_supp
-
+n_cores <- 10
 # model -------------------------------------------------------------------
 reps <- nrow(supplement_sample_df)
 sup_dist <- split(as.data.frame(supplement_sample_df),
@@ -150,9 +75,10 @@ N_sim <- mclapply(sup_dist,
                           supp = supplement,
                           stage.supp = 1,
                           n.supp = unname(unlist(param_sup)), 
-                          when.supp = seq_suplementation), mc.cores = 5)
+                          when.supp = seq_suplementation), mc.cores = n_cores)
 length(N_sim)
-system.time(sim_N <- mclapply(N_sim, function(x) em.extract_N(1,x), mc.cores = 5))
+system.time(sim_N <- mclapply(N_sim, function(x) em.extract_N(1,x), 
+                              mc.cores = n_cores))
 for (i in 1:length(N_sim)) sim_N[[i]]$run <- names(sim_N)[i]
 
 N_simulated <- do.call('rbind', sim_N)%>% 
@@ -163,7 +89,7 @@ nlevels(N_simulated$pop)
 levels(N_simulated$stage) <- stages
 levels(N_simulated$pop) <- grasslands
 
-
+## summarise ---------------
 sim_N_sum <- N_simulated %>% 
  # filter(stage != 'J') %>% 
   mutate(age = ifelse(stage == 'J', 'N_Juv', 'N_adult')) %>% 
@@ -196,7 +122,10 @@ sim_N_sum_supp <- supplement_sample_df %>%
 head(sim_N_sum_supp)
 
 
-# N ~ tsteps  -------------------------------------------------------------
+# plot --------------------------------------------------------------------
+
+
+## N ~ tsteps  -------------------------------------------------------------
 summary(1:100)
 quantile(1:100, 0.25)
 pg1 <- sim_N_sum_supp %>% 
@@ -222,7 +151,7 @@ ggsave('./figures/supplementation_440400_supp.png',
        plot = pg1, units = 'cm',
        height = 16, width = 20)
 
-# N ~ n.supp --------------------------------------------------------------
+## N ~ n.supp --------------------------------------------------------------
 sim_N_sum_supp_2050 <- sim_N_sum_supp %>% 
   filter(year == 2050) %>% 
   mutate(extinct = N < 2) %>% 
@@ -259,14 +188,14 @@ pg2 <- sim_N_sum_supp_2050 %>%
         panel.grid.minor.x = element_blank(),
         legend.position = 'none')+
   geom_smooth(method = 'gam', colour = 'black'); pg2
-plotly::ggplotly(pg2)
+#plotly::ggplotly(pg2)
 ggsave('./figures/supplementation_2050_supp.png',
        units = 'cm',
        height = 16, width = 20)
 
 # Ext ~ n.supp
-sim_N_sum_supp_2050 %>% 
-  filter(n_supp > 0) %>% 
+pg3 <- sim_N_sum_supp_2050 %>% 
+  filter(n_supp > 0, n_supp <101) %>% 
   mutate(prob_0 = ext_prob == 0) %>% 
   ggplot(aes(n_supp, ext_prob, colour = prob_0))+
   geom_line()+
@@ -275,12 +204,187 @@ sim_N_sum_supp_2050 %>%
   theme_bw()+
   geom_hline(yintercept = 0.05, lty = 2)+
  # scale_x_log10(breaks = supp_seq[!supp_seq %in% c(64, 160,36, 40, 100, 240, 500,180, 320, 360, 480)])+
-  scale_x_continuous(n.breaks = 20)+
+  scale_x_continuous(n.breaks = 10)+
   theme(panel.grid.major.x = element_blank(),
         panel.grid.minor.x = element_blank())+
   geom_vline(xintercept = 0)+
   geom_smooth(method = 'gam', colour = 'black')
 
-ggsave('./figures/supplementation_2050_supp_extprob.png',
+ggsave('./figures/supplementation_2050_supp_extprob.png', plot = pg3,
        units = 'cm',
        height = 16, width = 20)
+
+
+## extinction --------------------------------------------------------------
+supp_run_code <- supplement_sample_df %>% 
+  mutate(run = rownames(.),
+         supp = CA) %>% 
+  select(run, supp)
+
+extinction_prob <- sim_N_sum %>% 
+  mutate(extinct = N < 2,
+         year = tstep +2012) %>% 
+  left_join(supp_run_code) %>%
+  ungroup() %>% 
+  group_by(year, tstep,pop, supp) %>% 
+  summarise(sims = n(),
+            sim0 = sum(extinct),
+            ext_p = sum(extinct)/n())
+extinction_prob
+
+pg4<-extinction_prob %>% 
+  filter(year %in% seq(2025,2100, 10), supp > 10) %>% 
+  ggplot(aes(supp, ext_p, colour = pop))+
+  geom_point(alpha = 0.4)+
+  geom_smooth(method = 'gam')+
+  theme_bw()+
+  facet_wrap(~year, scale = 'free')+
+  ylab('Extinction probability')+
+  xlab('Yearly Supplementation (>10)')+
+  geom_hline(yintercept = 0.05, linetype = 'dashed');pg4
+
+ggsave('./figures/supplementation_extinction_prob.png', plot = pg4,
+       units = 'cm',
+       height = 16, width = 20)
+
+
+
+### gam model ---------------------------------------------------------------
+
+pg5<-extinction_prob %>% 
+  filter(year %in% seq(2035,2100, 10),
+         ext_p >0, supp < 101) %>% 
+  ggplot(aes(supp, ext_p, colour = pop))+
+  geom_point(alpha = 0.4)+
+  geom_smooth(method = 'gam', se = F, colour = 'grey30')+
+  geom_smooth(method = 'lm', linetype = 'dashed', se = F, colour = 'red')+
+  theme_bw()+
+  scale_y_log10()+
+  scale_x_log10()+
+  facet_grid(pop~year, scale = 'free')+
+  ylab('Extinction probability')+
+  xlab('Yearly Supplementation')+
+  geom_hline(yintercept = 0.05, linetype = 'dashed');pg5
+
+plotly::ggplotly(pg4)
+
+
+extinction_prob %>% 
+  ungroup() %>% 
+  filter(ext_p <= 0.05,
+         year == 2050) %>% 
+  arrange(pop,supp) %>% 
+  group_by(year, pop, ext_p) %>% 
+  summarise(max(supp))
+
+
+extinction_prob %>% 
+  filter(year %in% seq(2026,2100, 4),
+         ext_p >0) %>% 
+  ggplot(aes(supp, ext_p, colour = pop))+
+  geom_point(alpha = 0.4)+
+  geom_smooth(method = 'lm', se = F)+
+  #geom_smooth(method = 'lm', aes(group = year))+
+  theme_bw()+
+  scale_y_log10()+
+  scale_x_log10()+
+  facet_wrap(~year, scale = 'free_x')+
+  ylab('Extinction probability')+
+  xlab('Yearly Supplementation')+
+  geom_hline(yintercept = 0.05, linetype = 'dashed')
+
+# years > 33 seem to converge
+
+ext_data <- extinction_prob %>% 
+  filter(year > 2035,
+       #  ext_p >0, 
+         supp < 100) %>% 
+  mutate(logsupp = log(supp),
+         logextp = log(ext_p),
+         logitextp = logit(ext_p))
+
+pg6 <-  ggplot(ext_data,aes(supp, ext_p))+
+  geom_point(alpha = 0.1, colour = 'grey60')+
+  # geom_smooth(method = 'lm', se = F, colour = 'grey30')+
+  geom_smooth(method = 'gam', linetype = 'dashed',
+              aes(group = year, colour = year), se = F, linewidth = 0.2)+
+  theme_bw()+
+  #scale_y_log10()+
+  scale_x_log10()+
+  facet_wrap(~pop, scale = 'free_x')+
+  ylab('Extinction probability')+
+  xlab('Yearly Supplementation')+
+  geom_hline(yintercept = 0.05, linetype = 'dashed'); pg6
+
+
+ggsave('./figures/supplementation_extinction_prob_year.png', plot = pg6,
+       units = 'cm',
+       height = 16, width = 20)
+
+
+  lm(ext_p~logsupp*pop, data = ext_data[ext_data$year %in% c(2035:2040),]) %>% 
+    summary
+  
+  
+  gam_supp <- gam(ext_p ~ s(logsupp,
+                            by = interaction(pop, year), k = 10) + pop + year, 
+                  data = ext_data[ext_data$year %in% c(2035:2040),]) 
+  
+  summary(gam_supp)
+plot(gam_supp$residuals)
+  
+gam_supp <- gam(ext_p ~ s(logsupp,
+                          by = interaction(pop), k = 10) + pop, 
+                data = ext_data) 
+
+summary(gam_supp)
+plot(gam_supp$residuals)
+
+  ext_data$predicted <- predict(gam_supp)  
+
+ pg7 <-  ggplot(ext_data,aes(supp, ext_p, colour = pop))+
+    geom_point(alpha = 0.4)+
+    #geom_smooth(method = 'lm', se = F, colour = 'grey30')+
+    #geom_smooth(method = 'gam', colour = 'red', linetype = 'dashed')+
+    theme_bw()+
+    geom_line(aes(y = predicted), colour = 'black')+
+    #scale_y_log10()+
+    scale_x_log10()+
+    facet_wrap(~pop, scale = 'free_x')+
+    ylab('Extinction probability')+
+    xlab('Yearly Supplementation')+
+    geom_hline(yintercept = 0.05, linetype = 'dashed');pg7
+
+  
+ ggsave('./figures/supplementation_extinction_prob_gam.png', 
+        plot = pg7,
+        units = 'cm',
+        height = 16, width = 20)
+ 
+ xx <- seq(1, 60, 0.01)
+  pred_extdata <- data.frame(logsupp = log(xx), supp = xx,
+                                 pop = rep(levels(ext_data$pop), 
+                                           each = length(xx)))
+  pred_extdata$ext_p <- predict(gam_supp, newdata = pred_extdata)   
+
+extinction_table <-  pred_extdata %>% 
+    mutate(ext_p2 = round(ext_p, 2)) %>% 
+    filter(ext_p2 <= 0.1) %>% 
+    group_by(pop, ext_p2) %>% 
+    summarise(sup = round(mean(supp))) %>% 
+    pivot_wider(names_from = pop, values_from = sup) %>% 
+  rename(`Extinction Probability` = ext_p2)
+
+extinction_table %>% 
+  mutate_all(~replace(., is.na(.), '-')) %>% 
+  flextable() %>%
+  border_remove() %>% 
+  align(j = 1, align = 'center') %>% 
+  hline_bottom(border = fp_border_default(width = 2)) %>%  
+  hline(part = 'header', border = fp_border_default(width = 2)) %>% 
+  vline(j = 1, part = 'body', border = fp_border_default(width = 1.5)) %>% 
+  bold(part = 'header') %>% 
+  font(part = 'all', fontname='calibri') -> ext_fltb;ext_fltb
+
+saveRDS(ext_fltb, './output/supplementation_extinct_prob_tbl.rds') 
+  
